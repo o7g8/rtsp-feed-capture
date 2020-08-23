7fc311fe40bb129c270c7cc89d877204feba637f
Ubuntu <ubuntu@ip-172-31-37-167.eu-west-1.compute.internal>
2020-08-23 00:39:33 +0000

    Long blocking actor.

--- rtsp-capture-actors/ingest.py
+++ rtsp-capture-actors/ingest.py
@@ -1,15 +1,61 @@
 import cv2
+import logging
+import time
 from thespian.actors import *
 
-class Hello(Actor):
+def feed_system():
+    actorsys = ActorSystem()
+    #actorsys = ActorSystem("multiprocTCPBase")
+    frameProcessor = actorsys.createActor(FrameProcessor)
+    feed = actorsys.createActor(VideoFeed)
+    actorsys.ask(feed, ("readFeed", "rtsp://127.0.0.1:8554/test", "feed1", frameProcessor))
+    #time.sleep(60)
+    actorsys.shutdown()
+
+class VideoFeed(Actor):
+    def readFeed(self, url, name, frameProcessor):
+        cameraStream = cv2.VideoCapture(url)
+        fps = int(cameraStream.get(cv2.CAP_PROP_FPS))
+        i=0
+        while True:
+            if i % fps != 0:
+                valid = cameraStream.grab()
+                if not valid:
+                    print("Bad grab")
+                    break
+                i = i+1
+                continue
+            valid, frame = cameraStream.read()
+            if not valid:
+                print("End")
+                break
+            i = i +1
+            print("sending frame")
+            self.send(frameProcessor, ("processFrame", name, frame))
+
     def receiveMessage(self, message, sender):
-        self.send(sender, 'Hello, world!')
+        # readFeed(url, name)
+        if not isinstance(message, tuple):
+            logging.error("VideoFeed: wrong message format")
+            return
+        msgId, url, name, frameProcessor = message
+        if msgId != "readFeed":
+            logging.error("VideoFeed: wrong message {}".format(msgId))
+            return
+        self.readFeed(url, name, frameProcessor)
 
-def say_hello():
-    ActorSystem()
-    hello = ActorSystem().createActor(Hello)
-    print(ActorSystem().ask(hello, 'are you there?', 1.5))
-
+class FrameProcessor(Actor):
+    def receiveMessage(self, message, sender):
+        # processFrame(senderName, frame)
+        print("got msg")
+        if not isinstance(message, tuple):
+            logging.error("FrameProcessor: wrong message format")
+            return
+        msgId, senderName, frame = message
+        if msgId != "processFrame":
+            logging.error("FrameProcessor: wrong message {}".format(msgId))
+            return           
+        print("FrameProcessor: received a frame from {}".format(senderName))
 
 def video():
     cameraStream = cv2.VideoCapture("rtsp://127.0.0.1:8554/test")
@@ -35,5 +81,5 @@ def video():
 
 
 if __name__ == "__main__":
-    video()
+    feed_system()
     #say_hello()
